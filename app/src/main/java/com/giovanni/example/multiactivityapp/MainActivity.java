3392f5e7c30b6cb156b4f39bda248ac8279b3374
Giovanni <gbertao96@gmail.com>
2021-09-14 22:18:43 -0300

    Call the right activity

--- app/src/main/java/com/giovanni/example/multiactivityapp/MainActivity.java
+++ app/src/main/java/com/giovanni/example/multiactivityapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.giovanni.example.multiactivityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((Button)findViewById(R.id.btnAc1)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btnAc2)).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        switch(v.getId()) {
+            case R.id.btnAc1:
+                intent = new Intent(this, Activity1.class);
+                break;
+            case R.id.btnAc2:
+                intent = new Intent(this, Activity2.class);
+                break;
+            default:
+        }
+
+        if (intent != null)
+            startActivity(intent);
     }
 }
